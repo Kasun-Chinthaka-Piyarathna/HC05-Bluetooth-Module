8f56081ff2016147f6f41624359fa5482205acad
ChinthakaMora <kasunchinthaka555@gmail.com>
2018-06-03 16:38:32 +0530

    modified header file

--- hc06.h
+++ hc06.h
@@ -1,27 +1,6 @@
 //Micricontroller		:All 8-bit AVR Micricontrollers with Similar Register Configuration with ATmega16
 //System Clock			:8 MHz
 //Header File Version 		:1.1
-//Author			:Arun Kumar Garg 
-//				:ABLab Solutions
-//				:www.ablab.in
-//				:arun@ablab.in
-//Date				:1st June 2015
-
-/*Copyright (c) 2011 ABLab Solutions All rights reserved.
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-   * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 	following disclaimer in the documentation 
-     and/or other materials provided with the distribution.
-   * Neither the name of the copyright holders nor the names of contributors may be used to endorse or promote products derived from this software without 
-     specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
-
 
 /*The functions and macros defined in this header file are for HC-06 Bluetooth Module*/
 
